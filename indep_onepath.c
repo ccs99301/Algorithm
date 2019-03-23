74b6d48f636c8dec92d416d88b6dde8b4b7fb384
ccs99301 <cs99301@gmail.com>
2019-03-23 14:02:51 +0800

    Add files via upload

---
+++ indep_onepath.c
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int n,a[200200],dp1[200200],dp0[200200];
+// dp0 is a not select array for index.
+// dp1 is a select array for index.
+void solve(){
+	for(int i=1;i<=n;i++){
+		dp0[i]=max(dp0[i-1],dp1[i-1]); // find the best for choose last one or not. 
+		dp1[i]=dp0[i-1]+a[i]; // if choose dp1 is dp0 from last one and add up itself.
+	}
+	printf("%d\n",max(dp0[n],dp1[n]));
+}
+void init(){
+	scanf("%d",&n);
+	if(n==0) return ;
+	memset(dp1,0,sizeof(dp1));
+	memset(dp0,0,sizeof(dp0));
+	for(int i=1;i<=n;i++) scanf("%d",&a[i]);
+	dp1[0]=0;
+	dp0[0]=0;
+	return ;
+}
+int main(){
+	while(1){
+		init();
+		if(n==0) return 0;
+		solve();
+	}
+}
